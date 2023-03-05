ed62d70150c0f7ae19f1564db8bc15e2269873f0
Ben Cooper <ben.f.cooper@gmail.com>
2023-03-05 14:01:57 +0000

    improved button styling

--- popup.js
+++ popup.js
@@ -1,24 +1,41 @@
 let showShorts = false;
 
+const materialIconsLink = document.createElement("link");
+materialIconsLink.rel = "stylesheet";
+materialIconsLink.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
+document.head.appendChild(materialIconsLink);
+
 const toggleButton = document.createElement("button");
 document.body.appendChild(toggleButton);
-setInterval(()=>{
-if(window.location.href == "https://www.youtube.com/feed/subscriptions"){
-// Create toggle button
-toggleButton.style.display = "block";
-toggleButton.innerText = "Toggle Shorts";
-toggleButton.style.position = "fixed";
-toggleButton.style.bottom = "20px";
-toggleButton.style.right = "20px";
-toggleButton.style.zIndex = "9999";
-toggleButton.style.cursor = "pointer";
-
-}
-else{
-toggleButton.style.display = "none";
-
-}
-}, 1000)
+if (showShorts) {
+    toggleButton.style.backgroundColor = "#ff6666";
+  } else {
+    toggleButton.style.backgroundColor = "#f00";
+  }
+setInterval(() => {
+  if (window.location.href == "https://www.youtube.com/feed/subscriptions") {
+    // Create toggle button
+    toggleButton.style.display = "block";
+    toggleButton.innerHTML = "<i class='material-icons' style='padding-right: 5px;'>auto_awesome</i><span style='padding-left: 5px;'>TOGGLE SHORTS</span>";
+    toggleButton.style.color = "#fff";
+    toggleButton.style.border = "none";
+    toggleButton.style.borderRadius = "10px";
+    toggleButton.style.padding = "10px 16px";
+    toggleButton.style.fontWeight = "bold";
+    toggleButton.style.position = "fixed";
+    toggleButton.style.bottom = "20px";
+    toggleButton.style.right = "20px";
+    toggleButton.style.zIndex = "9999";
+    toggleButton.style.cursor = "pointer";
+    toggleButton.style.display = "flex";
+    toggleButton.style.flexDirection = "row";
+    toggleButton.style.justifyContent = "center";
+    toggleButton.style.alignItems = "center";
+    toggleButton.style.fontFamily = "Roboto, Arial, sans-serif";
+  } else {
+    toggleButton.style.display = "none";
+  }
+}, 1000);
 
 // Toggle function
 function toggleShorts() {
@@ -28,12 +45,12 @@ function toggleShorts() {
 
 function removeShorts() {
   // Get all the video items
-  const videoItems = document.querySelectorAll('#items > ytd-grid-video-renderer');
+  const videoItems = document.querySelectorAll("#items > ytd-grid-video-renderer");
 
   // Loop through each video item
   videoItems.forEach((item) => {
     // Check if the video item is a short video
-    const isShortVideo = item.querySelector('span.ytd-thumbnail-overlay-time-status-renderer');
+    const isShortVideo = item.querySelector("span.ytd-thumbnail-overlay-time-status-renderer");
     if (isShortVideo !== null) {
       // If it's a short video and showShorts is false, hide it from the feed
       if (isShortVideo.innerText.toLowerCase().includes("shorts") && !showShorts) {
@@ -51,10 +68,13 @@ function removeShorts() {
 toggleButton.addEventListener("click", toggleShorts);
 
 // Listen for scroll events and remove short videos if toggle is selected
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (!showShorts) {
     removeShorts();
   }
 });
 
-setTimeout(()=>{removeShorts();}, 1000)
+setTimeout(() => {
+  removeShorts();
+}, 1000);
+
